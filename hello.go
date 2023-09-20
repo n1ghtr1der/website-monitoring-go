31329c86f60e9643dcc587eac7ed71413e35b30c
Emirton Sousa <emirtonf@gmail.com>
2023-09-20 14:37:23 -0300

    feat: code splitted in new functions and os lib imported

--- hello.go
+++ hello.go
@@ -2,26 +2,43 @@ package main
 
 import (
 	"fmt"
+	"os" //essa biblioteca comunica a aplicação com o sistema operacional
 )
 
 func main() {
-	version := 1.0
-	fmt.Printf("This program is running on version %v\n", version)
 
+	showIntro()
+	showMenu()
+	command := readOption()
+
+	switch command {
+	case 1:
+		fmt.Println("Start monitoring")
+	case 2:
+		fmt.Println("Retrieving data...")
+	case 3:
+		fmt.Println("Goodbye")
+		os.Exit(0) // sai do programa sem erros
+	default:
+		fmt.Println("Unknown command... exiting application")
+		os.Exit(-1) //sai do programa retornando um erro
+	}
+}
+
+func showIntro() {
+	version := 1.0
+	fmt.Printf("This program is running on version %v\n\n", version)
+}
+
+func showMenu() {
 	fmt.Println("1 - Start monitoring")
 	fmt.Println("2 - Show logs")
 	fmt.Println("3 - Exit")
+}
 
+func readOption() int {
 	var option int
 	fmt.Scan(&option)
 
-	if option == 1 {
-		fmt.Println("Start monitoring")
-	} else if option == 2 {
-		fmt.Println("Retrieving logs...")
-	} else if option == 3 {
-		fmt.Println("Goodbye")
-	} else {
-		fmt.Println("Unknown command...")
-	}
+	return option
 }
